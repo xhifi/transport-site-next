36c1a5b85700cf4fce4a39b2ad9372eac0b19525
shifi <shifa.newversion@gmail.com>
2023-11-06 23:27:13 +0500

    features component initialized

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,13 @@ import ServiceSprite from "@/components/ServiceSprite";
 import Card from "@/components/core/Card";
 import Footer from "@/components/footer";
 import Services from "@/components/services";
+import Features from "@/components/features";
 
 export default function Home() {
   return (
     <main>
       <Services />
+      <Features />
       <Footer />
     </main>
   );
---
+++ src/components/features/index.jsx
@@ -0,0 +1,4 @@
+const index = () => {
+  return <div>Featured content goes here...</div>;
+};
+export default index;
